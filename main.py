bb8a5ff941f12f5796400b8aac1c421f40d783e8
Thandar Khine Aye <thandarkhineaye92@gmail.com>
2021-10-01 14:48:39 +0900

    final commit

---
+++ main.py
@@ -0,0 +1,160 @@
+from flask import Flask, request, render_template
+import os
+import random
+import redis
+import socket
+import sys
+import logging
+from datetime import datetime
+
+# App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+
+# Logging
+logger = logging.getLogger(__name__)
+handler = AzureLogHandler(connection_string='InstrumentationKey=03d3c628-ee31-4ec6-8f2d-0f631355e677;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/')
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+# Metrics
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=03d3c628-ee31-4ec6-8f2d-0f631355e677;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/')
+
+# Tracing
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=03d3c628-ee31-4ec6-8f2d-0f631355e677;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/'),
+    sampler=ProbabilitySampler(1.0),
+)
+
+# Event logger
+eventlogger = logging.getLogger(__name__)
+eventhandler = AzureEventHandler(connection_string='InstrumentationKey=03d3c628-ee31-4ec6-8f2d-0f631355e677;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/')
+eventlogger.addHandler(eventhandler)
+eventlogger.setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+# Requests
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=03d3c628-ee31-4ec6-8f2d-0f631355e677;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+
+# Load configurations from environment or config file
+app.config.from_pyfile('config_file.cfg')
+
+if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
+    button1 = os.environ['VOTE1VALUE']
+else:
+    button1 = app.config['VOTE1VALUE']
+
+if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
+    button2 = os.environ['VOTE2VALUE']
+else:
+    button2 = app.config['VOTE2VALUE']
+
+if ("TITLE" in os.environ and os.environ['TITLE']):
+    title = os.environ['TITLE']
+else:
+    title = app.config['TITLE']
+
+# Redis Connection
+#r = redis.Redis()
+# Comment/remove the next two lines of code.
+# Redis Connection to a local server running on the same machine where the current FLask app is running. 
+# r = redis.Redis()
+# Redis configurations
+redis_server = os.environ['REDIS']
+
+# Redis Connection to another container
+try:
+   if "REDIS_PWD" in os.environ:
+      r = redis.StrictRedis(host=redis_server,
+                        port=6379,
+                        password=os.environ['REDIS_PWD'])
+   else:
+      r = redis.Redis(redis_server)
+   r.ping()
+except redis.ConnectionError:
+   exit('Failed to connect to Redis, terminating.')
+
+
+# Change title to host name to demo NLB
+if app.config['SHOWHOST'] == "true":
+    title = socket.gethostname()
+
+# Init Redis
+if not r.get(button1): r.set(button1,0)
+if not r.get(button2): r.set(button2,0)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+
+        # Get current values
+        vote1 = r.get(button1).decode('utf-8')
+        tracer.span(name="Cats Vote")
+
+        vote2 = r.get(button2).decode('utf-8')
+        tracer.span(name="Dogs Vote")
+
+        # Return index with values
+        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+    elif request.method == 'POST':
+
+        if request.form['vote'] == 'reset':
+
+            # Empty table and return results
+            r.set(button1,0)
+            r.set(button2,0)
+            vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info('cat vote', extra=properties)
+
+            vote2 = r.get(button2).decode('utf-8')
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info('dog vote', extra=properties)
+
+            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+        else:
+
+            # Insert vote result into DB
+            vote = request.form['vote']
+            r.incr(vote,1)
+
+
+            #vote0 = r.get(vote).decode('utf-8')
+            
+            # log current vote
+            #properties = {'custom_dimensions': {'{}_vote'.format(vote): vote0}}
+            #logger.info('new_{}_vote'.format(vote), extra=properties)
+
+            # Get current values
+            vote1 = r.get(button1).decode('utf-8')
+            vote2 = r.get(button2).decode('utf-8')
+
+            # Return results
+            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+if __name__ == "__main__":
+    # comment line below when deploying to VMSS
+    app.run() # local
+    # uncomment the line below before deployment to VMSS
+    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
